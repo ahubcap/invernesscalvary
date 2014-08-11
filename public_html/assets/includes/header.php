16ed6bb85ab8a939cfe7c87f3594a947b74b58d0
Alexander Parker <alexp@invernesscalvary.com>
2014-08-10 21:32:54 -0400

    Beginning menu implementation

--- public_html/assets/includes/header.php
+++ public_html/assets/includes/header.php
@@ -30,12 +30,12 @@
 
 <div id="mobile_bar">
     <div id="top_fixed">
-        <div id="mobile_menu">
+        <nav><div id="mobile_menu">
             <ul id="mobile_nav">
                 <li id="menu" class="current">&#9776;</li>
                 <?php $dom = "nav"; include 'assets/includes/nav.php'; ?>
             </ul>
-        </div>
+            </div></nav>
     </div>
     <header id="main_header">
         <div id="drop_nav"></div>
